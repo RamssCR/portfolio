ffa16e0a403ea9fef2777bf1bfe15fd5e53d8225
RamssCR <ramsesgpm@gmail.com>
2025-11-25 22:34:24 -0500

    feat: created extra utility functions

---
+++ src/schemas/env.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const env = z.object({
+  VITE_VERSION: z.string().optional(),
+  MODE: z.enum(['development', 'production', 'staging', 'test']).default('development'),
+  TEST: z.string(),
+  DEV: z.boolean(),
+})
+
+export type Env = z.infer<typeof env>
